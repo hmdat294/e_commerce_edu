9ecaf2aa8dbfa26a43ff6b009c08eee3122ed363
hmdat294 <hodat2092004@gmail.com>
2025-07-10 14:51:10 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './routes/Home';
+import About from './routes/About';
+import NotFound from './routes/NotFound';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
